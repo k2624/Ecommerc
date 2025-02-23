c5896c11cd3564b76352d3c826b726a91e5ce462
ن2624 <khaderelhabal923@gmail.com>
2025-02-23 10:19:45 +0200

    Fix image paths for GitHub Pages

--- src/Components/MainSlider/MainSlider.jsx
+++ src/Components/MainSlider/MainSlider.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import style from './MainSlider.module.css';
-import slide1 from '../../assets/images/slider-image-1.jpeg';
-import slide2 from '../../assets/images/slider-image-2.jpeg';
-import slide3 from '../../assets/images/slider-image-3.jpeg';
-import banner1 from '../../assets/images/banner-4.jpeg';
-import banner2 from '../../assets/images/grocery-banner-2.jpeg';
-import Slider from 'react-slick';
+import React from "react";
+const slide1 = `${import.meta.env.BASE_URL}assets/images/slider-image-1.jpeg`;
+const slide2 = `${import.meta.env.BASE_URL}assets/images/slider-image-2.jpeg`;
+const slide3 = `${import.meta.env.BASE_URL}assets/images/slider-image-3.jpeg`;
+const banner1 = `${import.meta.env.BASE_URL}assets/images/banner-4.jpeg`;
+const banner2 = `${
+  import.meta.env.BASE_URL
+}assets/images/grocery-banner-2.jpeg`;
+
+import Slider from "react-slick";
 
 // زر السابق
 const PrevArrow = ({ onClick }) => (
-  <button className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10" onClick={onClick}>
+  <button
+    className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10"
+    onClick={onClick}
+  >
     ❮
   </button>
 );
 
 // زر التالي
 const NextArrow = ({ onClick }) => (
-  <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10" onClick={onClick}>
+  <button
+    className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10"
+    onClick={onClick}
+  >
     ❯
   </button>
 );
@@ -37,15 +45,27 @@ export default function MainSlider() {
   return (
     <div className="flex mb-10 relative">
       <div className="w-3/4 relative">
-        <Slider {...settings} >
-          <img src={slide1} className="w-full object-cover object-right h-[400px]" />
-          <img src={slide2} className="w-full object-cover object-right h-[400px]" />
-          <img src={slide3} className="w-full object-cover object-right h-[400px]" />
+        <Slider {...settings}>
+          <img
+            src={slide1}
+            className="w-full object-cover object-right h-[400px]"
+          />
+          <img
+            src={slide2}
+            className="w-full object-cover object-right h-[400px]"
+          />
+          <img
+            src={slide3}
+            className="w-full object-cover object-right h-[400px]"
+          />
         </Slider>
       </div>
       <div className="w-1/4">
         <img src={banner1} className="w-full object-cover h-[200px]" />
-        <img src={banner2} className="w-full object-cover object-right h-[200px]" />
+        <img
+          src={banner2}
+          className="w-full object-cover object-right h-[200px]"
+        />
       </div>
     </div>
   );
